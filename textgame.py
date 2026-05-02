601dbf5d875cf441ddc5b1958d983dd5d13dd411
sonakim <aaa896516@gmail.com>
2026-05-03 07:40:58 +0900

    2

--- textgame.py
+++ textgame.py
@@ -1,7 +1,41 @@
-location="연대앞 정류장"
 condition="배고프다"
 time="11시"
 
-print("현재 위치:",location)
+campus=[["공학관","백양로1","백주년기념관"],["공학원","백양로1","공터1"],["연대앞 버스정류장","정문","세븐란스병원 버스정류장"]]
+
+row=2
+col=0
+
+
+print("현재 위치:",campus[row][col])
 print("현재 상태:",condition)
 print("현재 시각:",time)
+
+while True:
+    direction=input("이동 방향(동,서,남,북): ")
+
+    if direction =="동":
+        if col+1 >= 3:
+            print("그 방향은 막혔어")
+        else:
+            col=col+1
+
+    elif direction =="서":
+        if col-1 < 0:
+            print("그 방향은 막혔어")
+        else:
+            col=col-1
+
+    elif direction =="남":
+        if row+1 >= 3:
+            print("그 방향은 막혔어")
+        else:
+                row=row+1
+
+    elif direction =="북":
+        if row-1 < 0:
+            print("그 방향은 막혔어")
+        else:
+            row=row-1
+         
+    print("현재 위치:",campus[row][col])
